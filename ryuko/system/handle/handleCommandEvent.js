eac96b7448a76f3256777be2e2f7d19d81c181cc
jaylordlapena21-afk <jaylordlapena21@gmail.com>
2025-09-18 01:42:59 +0800

    Update handleCommandEvent.js

--- ryuko/system/handle/handleCommandEvent.js
+++ ryuko/system/handle/handleCommandEvent.js
@@ -1,42 +1,51 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    const logger = require("../../catalogs/ryukoc.js")
+    const logger = require("../../catalogs/ryukoc.js");
     return function ({ event }) {
         const { allowInbox } = global.ryuko;
         const { userBanned, threadBanned } = global.data;
         const { commands, eventRegistered } = global.client;
-        var { senderID, threadID } = event;
-        var senderID = String(senderID);
-        var threadID = String(threadID);
-        if (userBanned.has(senderID) || threadBanned.has(threadID) || allowInbox == !![] && senderID == threadID) return;
+        let { senderID, threadID } = event;
+        senderID = String(senderID);
+        threadID = String(threadID);
+
+        // Check if user or thread is banned, or if inbox is not allowed and sender is the same as thread
+        if (userBanned.has(senderID) || threadBanned.has(threadID) || (!allowInbox && senderID === threadID)) return;
+
+        // Iterate over all registered events for matching event types
         for (const eventReg of eventRegistered) {
             const cmd = commands.get(eventReg);
-            var getText2;
+            let getText2;
 
-            if (cmd.languages && typeof cmd.languages == 'object') 
+            if (cmd.languages && typeof cmd.languages === 'object') {
                 getText2 = (...values) => {
-                const commandModule = cmd.languages || {};
-                if (!commandModule.hasOwnProperty(global.config.language)) 
-                    return api.sendMessage(global.getText('handleCommand','notFoundLanguage', cmd.config.name), threadID, messengeID); 
-                var lang = cmd.languages[global.config.language][values[0]] || '';
-                for (var i = values.length; i > 0x16c0 + -0x303 + -0x1f * 0xa3; i--) {
-                    const expReg = RegExp('%' + i, 'g');
-                    lang = lang.replace(expReg, values[i]);
-                }
-                return lang;
-            };
-            else getText2 = () => {};
+                    const commandModule = cmd.languages || {};
+                    if (!commandModule.hasOwnProperty(global.config.language)) {
+                        return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', cmd.config.name), threadID, event.messageID); 
+                    }
+                    let lang = cmd.languages[global.config.language][values[0]] || '';
+                    for (let i = values.length; i > 0; i--) {
+                        const expReg = RegExp('%' + i, 'g');
+                        lang = lang.replace(expReg, values[i]);
+                    }
+                    return lang;
+                };
+            } else {
+                getText2 = () => {};
+            }
+
             try {
-                const Obj = {};
-                Obj.event = event 
-                Obj.api = api
-                Obj.models = models
-                Obj.Users = Users
-                Obj.Threads = Threads 
-                Obj.Currencies = Currencies 
-                Obj.getText = getText2;
+                const Obj = {
+                    event: event,
+                    api: api,
+                    models: models,
+                    Users: Users,
+                    Threads: Threads,
+                    Currencies: Currencies,
+                    getText: getText2
+                };
                 if (cmd) cmd.handleEvent(Obj);
             } catch (error) {
-                logger(global.getText('handleCommandEvent', 'moduleError', cmd.config.name), 'error');
+                logger(`Error in handling event: ${cmd.config.name} - ${JSON.stringify(error)}`, 'error');
             }
         }
     };
